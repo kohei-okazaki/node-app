5448ea7e096cc99f905d566b28cad212dfedb090
kou1210hei <kou1210hei@gmail.com>
2019-09-17 22:39:54 +0900

    no message

--- kin-dashboard/routes/login/index.js
+++ kin-dashboard/routes/login/index.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const router = express.Router();
+const bodyParser = require("body-parser")
 const { check, validationResult } = require('express-validator');
 
 /** 
- * ログイン画面表示 
+ * ログイン画面表示
+ * @param リクエストURL
+ * @param function (req, res) ログイン要求処理
  */
-router.get('/index', function (req, res, next) {
-    console.log("あいうえお");
+router.get('/index', function (req, res) {
     let view = {
+        // ログインformを初期化
         form: {
             loginId: '',
             password: ''
@@ -18,24 +21,38 @@ router.get('/index', function (req, res, next) {
 
 /** 
  * ログイン処理
+ * @param リクエストURL
+ * @param Validate[] 妥当性チェック情報の配列
+ * @param function (req, res) ログイン要求処理
  */
 router.post('/top', [
-    check('loginId').isEmpty(), 
-    check('password').isEmpty()
+    check('loginId')
+        .isEmpty().withMessage('ログインIDの入力は必須です'),
+    check('password')
+        .isEmpty().withMessage('パスワードの入力は必須です')
 ], function (req, res) {
 
     const errors = validationResult(req);
+
     if (!errors.isEmpty()) {
         // 妥当性チェックエラーが発生した場合
         console.log("妥当性チェックエラーが発生しました");
-    };
-    let view = {
-        form: {
-            loginId: '',
-            password: ''
+        const errorArray = errors.array();
+        console.log(errorArray.length);
+        for (var i = 0; i < errorArray.length; i++) {
+            console.log("エラーメッセージ=" + errorArray[i].msg);
         }
-    };
-    res.render('login/top', view);
+        let view = {
+            form: {
+                loginId: req.body.loginId,
+                password: req.body.password,
+                errorMessage: errorArray[0].msg
+            }
+        };
+        res.render('login/index', view);
+    } else {
+        res.render('login/top', {});
+    }
 });
 
 
